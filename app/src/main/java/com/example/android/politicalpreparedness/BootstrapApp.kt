a15e9a106ff3903bc9b7a40491f386b4918a96a0
Parzival <parzival.nieminen@gmail.com>
2021-05-18 16:03:30 +0200

    feat add app permissions and using new theme
    * add app permissions
    * add custom theme to the app
    * refactor name CapstoneApp.kt to BootstrapApp.kt

--- app/src/main/java/com/example/android/politicalpreparedness/BootstrapApp.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/BootstrapApp.kt
@@ -2,31 +2,27 @@ package com.example.android.politicalpreparedness
 
 import android.app.Application
 import com.example.android.politicalpreparedness.database.ElectionDatabase
-import com.example.android.politicalpreparedness.election.ElectionsViewModel
-import com.example.android.politicalpreparedness.election.VoterInfoViewModel
-import com.example.android.politicalpreparedness.representative.RepresentativeViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import timber.log.Timber
 
-class CapstoneApp : Application() {
+class BootstrapApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
 
         val capstoneAppModules = module {
-            single { ElectionDatabase.getInstance(this@CapstoneApp) }
+            single { ElectionDatabase.getInstance(this@BootstrapApp) }
             single { (get() as ElectionDatabase).electionDao }
         }
 
         startKoin {
-            androidContext(this@CapstoneApp)
+            androidContext(this@BootstrapApp)
             androidLogger()
-            modules(capstoneAppModules)
+            //modules(capstoneAppModules)
         }
     }
 
