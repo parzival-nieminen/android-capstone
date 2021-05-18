28db5a8b692223b6346768e2581424a65e2c3676
Parzival <parzival.nieminen@gmail.com>
2021-05-18 15:49:56 +0200

    feat  main activity update
    * using custome theme in the app
    * refactor to FragmentContainerView
    * clean up unused dependencies

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Timber.i("Main Activity onCreate is running")
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.launchFragment))
+
+        val navHostFragment =
+            this.supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return this.navController.navigateUp()
     }
 }
