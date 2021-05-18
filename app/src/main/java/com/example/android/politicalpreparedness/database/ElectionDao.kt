86df1f1873fea58ec458a7f907addfc36d25a086
Parzival <parzival.nieminen@gmail.com>
2021-05-18 17:27:33 +0200

    fix serialisation

--- app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDao.kt
@@ -1,5 +1,6 @@
 package com.example.android.politicalpreparedness.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.android.politicalpreparedness.network.models.Election
 
@@ -9,14 +10,14 @@ interface ElectionDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(election: Election)
 
-    @Query("SELECT * FROM election_table")
-    fun selectAll(): List<Election>
+    @Query("SELECT * FROM election_table ORDER BY electionDay ASC")
+    fun selectAll(): LiveData<List<Election>>
 
-    @Query("SELECT * FROM election_table WHERE id == :id")
-    fun selectById(id: Int): Election?
+    @Query("SELECT * FROM election_table WHERE id = :id")
+    fun selectById(id: Int): Election
 
-    @Delete
-    fun delete(election: Election)
+    @Query("DELETE FROM election_table WHERE id = :id")
+    fun delete(id: Int)
 
     @Query("DELETE FROM election_table")
     fun deleteAll()
